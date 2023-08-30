75f0d1cb2679f859006ae8efd74d771a7dcd25ca
jack-njenga <jacknjenga99@gmail.com>
2023-08-30 12:40:25 +0300

    puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # ssh private key config using puppet
-file { 'Identity File':
-  path	  => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school'
-  replace => true,
+
+include stdlib
+file_line{'Identity file':
+path => '/etc/ssh/config',
+line => 'IdentityFile ~/.ssh/school'
 }
 
-file_line { 'disable passwd authentication':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no'
-  replace => true,
+file_line{'Disable password authentication':
+path => '/etc/ssh/config',
+line => 'PasswordAuthentication no'
 }
