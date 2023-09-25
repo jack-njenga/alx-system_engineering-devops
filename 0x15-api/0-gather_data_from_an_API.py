af12cb206a79b2f12a6c853249c9230d2ce29c01
jack-njenga <jacknjenga99@gmail.com>
2023-09-25 20:05:27 +0300

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,32 +2,28 @@
 """
 Returns information about his/her TODO list progress.
 """
+
 import requests
 import sys
 
 if __name__ == "__main__":
-    emp_id = sys.argv[1]
-    try:
-        int_id = int(emp_id)
-    except Exception as e:
-        # print("agrument is not an integer")
-        pass
-
     url = "https://jsonplaceholder.typicode.com/"
     response = requests.get(f"{url}todos/").json()
+
+    todo_list = []
     count = 0
-    obj_list = []
 
-    for obj in response:
-        if int(obj["userId"]) == int_id:
-            if obj["completed"]:
-                obj_list.append(obj)
+    for todo in response:
+        if int(todo["userId"]) == int(sys.argv[1]):
+            if todo["completed"]:
+                todo_list.append(todo)
             else:
-                count += 1
+                count = count + 1
 
-    tt = len(obj_list) + count
-    user = requests.get(f"{url}users/{str(emp_id)}")
-    name = user.json()["name"]
-    print(f"Employee {name} is done with tasks({len(obj_list)}/{tt}):")
-    for obj in obj_list:
-        print(f"\t {obj['title']}")
+    tt = len(todo_list) + count
+    user = requests.get(f"{url}users/{str(sys.argv[1])}").json()
+    name = user["name"]
+
+    print(f"Employee {name} is done with tasks({len(todo_list)}/{tt}):")
+    for todo in todo_list:
+        print(f"\t {todo['title']}")
